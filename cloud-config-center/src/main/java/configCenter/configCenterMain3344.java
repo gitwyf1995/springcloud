fe86d3c487fa372cdc708bfcef5544d2a46a8c0e
wang1995 <kingyinfeng1995@163.com>
2022-07-20 15:27:09 +0800

    7.20

--- cloud-config-center/src/main/java/configCenter/configCenterMain3344.java
+++ cloud-config-center/src/main/java/configCenter/configCenterMain3344.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class configCenterMain3344 {
     public static void main(String[] args) {
         SpringApplication.run(configCenterMain3344.class,args);
--- cloud-config-client-3355/src/main/java/configClient/controller/configClientController.java
+++ cloud-config-client-3355/src/main/java/configClient/controller/configClientController.java
@@ -2,11 +2,17 @@ package configClient.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @RefreshScope  具备刷新的能力
+ */
 @RestController
 @Slf4j
+@RefreshScope
 public class configClientController {
 
     @Value("${config.info}")
